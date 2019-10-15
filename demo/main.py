c13afc33753f9538b3fb0747f8366b890c80d50b
huangzhengyue <huangzhengyue.1996@gmail.com>
2019-10-15 21:28:32 +0800

    update demo

--- demo/main.py
+++ demo/main.py
@@ -1,17 +1,16 @@
-from easy_py_server import httpd, Request, Response
-import json
+from easy_py_server import httpd, Request, Response, MultipartFile
 
 
 # get method
 @httpd.get("/api")
 def demo(a: int, b: int):
-    return json.dumps({"success": True, "content": "%d + %d = %d" % (a, b, a + b)})
+    return dict(success=True, content="%d + %d = %d" % (a, b, a + b))
 
 
 # path parameter
 @httpd.get("/api/:id")
 def demo(id):
-    return 'api ' + id
+    return 'api' + id
 
 
 # set session
@@ -32,6 +31,14 @@ def query(request: Request):
 @httpd.post("/post")
 def post(key):
     return str(key)
+
+
+# post multipart file
+@httpd.post("/multipart")
+def post(save_name: str, file: MultipartFile):
+    save_path = '{}.txt'.format(save_name)
+    file.save(save_path)
+    return dict(success=True, message="save to {}".format(save_path))
 
 
 if __name__ == '__main__':
